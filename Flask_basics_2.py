f7c1694fc36215b4e112465a0349b291dccd0c1f
Charlie <58361242+charlie-aashutosh@users.noreply.github.com>
2020-10-15 08:01:04 -0700

    Add files via upload

---
+++ Flask_basics_2.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app= Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1> Home Page </h1>"
+
+@app.route("/about")
+def about():
+    return "<h1> About Page </h1>"
+
+
+if __name__ == "__main__":
+    app.run()
